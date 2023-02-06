dd93b58ce566bd56660e2af0ef4843a833dde3ef
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-06 18:01:32 +0300

    добавил хедер

---
+++ src/assets/components/ui/Header/Header.tsx
@@ -0,0 +1,8 @@
+import cn from 'classnames';
+import styles from './Header.module.scss';
+
+const Header = () => {
+  return <header className={cn(styles.appHeader)}></header>;
+};
+
+export default Header;
