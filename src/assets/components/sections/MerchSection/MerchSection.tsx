35cc517bdeb01af7a4eb083f2e5b08810a1220be
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-08 22:08:18 +0300

    обновил секцию с мерчом

--- src/assets/components/sections/MerchSection/MerchSection.tsx
+++ src/assets/components/sections/MerchSection/MerchSection.tsx
@@ -4,18 +4,26 @@ import styles from './MerchSection.module.scss';
 import { MerchSectionProps } from './MerchSection.props';
 import useLocalization from '@hooks/useLocalization';
 import shopPhoto from '@media/merch shop/shop.png';
+import getUiSx from '@utils/getUiSx/getUiSx';
 
 const MerchSection: FC<MerchSectionProps> = ({}) => {
   const loc = useLocalization();
 
   return (
-    <section className={cn(styles.merch)}>
+    <section
+      style={getUiSx({
+        gap: 0,
+      })}
+      className={cn(styles.merch)}
+    >
       <div className={cn(styles.placeholder)}>
         <h2>{loc.merchSection.header}</h2>
 
         <img alt={'shop-photo'} src={shopPhoto} />
+      </div>
 
-        <div className={cn(styles.desc)}>{loc.merchSection.desc}</div>
+      <div className={cn(styles.placeholder, styles.desc)}>
+        {loc.merchSection.desc}
       </div>
     </section>
   );
