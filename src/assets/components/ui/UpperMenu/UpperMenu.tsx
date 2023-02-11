1c8a55b7d58d5995927c6e8ebca3b32fb3d7f99b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-11 13:25:54 +0300

    добавил адаптив для верхнего меню

--- src/assets/components/ui/UpperMenu/UpperMenu.tsx
+++ src/assets/components/ui/UpperMenu/UpperMenu.tsx
@@ -2,13 +2,24 @@ import cn from 'classnames';
 import styles from './UpperMenu.module.scss';
 import getUiSx from '@utils/getUiSx/getUiSx';
 import useLocalization from '@hooks/useLocalization';
+import { AppSettings } from '@redux/reducers/appSettingsSlice';
+import { useSelector } from 'react-redux';
+import { StoreType } from '@redux/types/redux-types';
 
 const UpperMenu = () => {
   const loc = useLocalization();
+  const { language }: AppSettings = useSelector(
+    (state: StoreType) => state.appSettings,
+  );
 
   return (
     <nav
-      className={cn(styles.menu)}
+      className={cn(
+        styles.menu,
+        styles.localization,
+        language === 'ru' ? styles.ru : '',
+        language === 'kz' ? styles.kz : '',
+      )}
       style={getUiSx({
         maxWidth: 'unset',
         gap: 0,
