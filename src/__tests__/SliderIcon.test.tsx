44d7965c164c061485baf9c60ad3de7da1e74179
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-17 14:31:41 +0300

    написал функцию для тестирования Redux

---
+++ src/__tests__/SliderIcon.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest';
+import SliderIcon from '@ui/SliderIcon/SliderIcon';
+import renderWithProvider from '@utils/renderWithProvider';
+
+describe('Slider icon', () => {
+  test('Render with index', () => {
+    const component = renderWithProvider(<SliderIcon index={0} />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
