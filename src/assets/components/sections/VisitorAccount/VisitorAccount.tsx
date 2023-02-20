f6e282f534f8fbc1dcfdc745ce4c0d8c166726bb
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-20 23:47:09 +0300

    добавил логику подсчета бонусов

--- src/assets/components/sections/VisitorAccount/VisitorAccount.tsx
+++ src/assets/components/sections/VisitorAccount/VisitorAccount.tsx
@@ -1,12 +1,60 @@
 import cn from 'classnames';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import styles from './VisitorAccount.module.scss';
 import { VisitorAccountProps } from './VisitorAccount.props';
 import getUiSx from '@utils/getUiSx';
 import useLocalization from '@hooks/useLocalization';
+import { summary } from '@utils/math-utils';
 
 const VisitorAccount: FC<VisitorAccountProps> = ({}) => {
   const loc = useLocalization();
+
+  type Bonus = {
+    value: number,
+    purpose: 'visit',
+    fireDate: string,
+  };
+
+  const getTotalBonuses = (bonuses: Bonus[]): number => {
+    return summary(...bonuses.map((bonus) => bonus.value));
+  };
+
+  const mockBonuses: Bonus[] = [
+    {
+      value: 12,
+      purpose: 'visit',
+      fireDate: '01.01.2014',
+    },
+    {
+      value: 12,
+      purpose: 'visit',
+      fireDate: '01.01.2014',
+    },
+    {
+      value: 12,
+      purpose: 'visit',
+      fireDate: '01.01.2014',
+    },
+    {
+      value: 12,
+      purpose: 'visit',
+      fireDate: '01.01.2014',
+    },
+    {
+      value: 12,
+      purpose: 'visit',
+      fireDate: '01.01.2014',
+    },
+    {
+      value: 12,
+      purpose: 'visit',
+      fireDate: '01.01.2014',
+    },
+  ];
+
+  useEffect(() => {
+    console.log({ totalBonuses: getTotalBonuses(mockBonuses) });
+  }, []);
 
   return (
     <div
