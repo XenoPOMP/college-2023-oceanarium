7a35a809865c00ec8d1cc56b5e396cafca0c3a16
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-20 22:25:01 +0300

    Revert "добавил файл .env"
    This reverts commit 6cb1d2429ca785cf2da033e964431cfc8ffd4621.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
-import { defineConfig, loadEnv, searchForWorkspaceRoot } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    // @ts-ignore
-    test: {
-      globals: true,
-      environment: 'jsdom',
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  // @ts-ignore
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
