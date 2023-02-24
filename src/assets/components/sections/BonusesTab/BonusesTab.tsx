1c6dbef2075ec5dab2ed7e09849105df3388e35a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-24 16:12:00 +0300

    вынес блок с бонусами в отдельный компонент

---
+++ src/assets/components/sections/BonusesTab/BonusesTab.tsx
@@ -0,0 +1,67 @@
+import cn from 'classnames';
+import { FC } from 'react';
+import styles from './BonusesTab.module.scss';
+import { BonusesTabProps } from './BonusesTab.props';
+import { summary } from '@utils/math-utils';
+import { Bonus } from '@sections/VisitorAccount/VisitorAccount';
+import { useQuery } from 'react-query';
+import { VisitorService } from '../../../../services/Visitor.service';
+import useLocalization from '@hooks/useLocalization';
+import useAuth from '@hooks/useAuth';
+import CircleLoader from '@ui/CircleLoader/CircleLoader';
+import VisitorBonus from '@ui/VisitorBonus/VisitorBonus';
+import { AxiosError } from 'axios/index';
+import parentStyles from '@components/sections/VisitorAccount/VisitorAccount.module.scss';
+
+const BonusesTab: FC<BonusesTabProps> = ({}) => {
+  const loc = useLocalization();
+  const { _uid, isLogged } = useAuth();
+  const { isLoading, data, error } = useQuery(
+    'get visitor bonuses for tab',
+    () => VisitorService.getUserBonuses(_uid),
+    {
+      enabled: isLogged,
+    },
+  );
+
+  const getTotalBonuses = (): number => {
+    if (data) {
+      return summary(...data?.data.map((bonus: Bonus) => bonus.bonus_amount));
+    } else return 0;
+  };
+
+  return <>
+    {isLoading ? (
+      <div className={cn(parentStyles.serverMessagePlaceholder)}>
+        <CircleLoader />
+      </div>
+    ) : !error && <>
+      <div className={cn(styles.totalBonuses)}>
+        <h4>
+          {loc.accountPage.visitor.totalBonusesText.replace(
+            'XXXX',
+            `${getTotalBonuses()}`,
+          )}
+        </h4>
+      </div>
+
+      <div className={cn(styles.bonusList)}>
+        {data?.data.map((bonus: Bonus, index: number) => (
+          <VisitorBonus bonus={bonus} key={`bonus-${index}`} />
+        ))}
+      </div>
+    </>}
+
+    {error && (
+      <div className={cn(parentStyles.serverMessagePlaceholder)}>
+        {(error as AxiosError).code === 'ERR_NETWORK'
+          && loc.accountPage.messages.noConnectionToServer}
+
+        {(error as AxiosError).code === '500'
+          && loc.accountPage.messages.internalServerErrorMessage}
+      </div>
+    )}
+  </>;
+};
+
+export default BonusesTab;
--- src/assets/components/sections/VisitorAccount/VisitorAccount.tsx
+++ src/assets/components/sections/VisitorAccount/VisitorAccount.tsx
@@ -1,16 +1,11 @@
 import cn from 'classnames';
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
 import styles from './VisitorAccount.module.scss';
 import { VisitorAccountProps } from './VisitorAccount.props';
 import getUiSx from '@utils/getUiSx';
 import useLocalization from '@hooks/useLocalization';
-import { summary } from '@utils/math-utils';
 import useAuth from '@hooks/useAuth';
-import { useQuery } from 'react-query';
-import { VisitorService } from '../../../../services/Visitor.service';
-import CircleLoader from '@ui/CircleLoader/CircleLoader';
-import VisitorBonus from '@ui/VisitorBonus/VisitorBonus';
-import { AxiosError } from 'axios';
+import BonusesTab from '@sections/BonusesTab/BonusesTab';
 
 export type Bonus = {
   bonus_amount: number,
@@ -20,26 +15,12 @@ export type Bonus = {
 
 const VisitorAccount: FC<VisitorAccountProps> = ({}) => {
   const loc = useLocalization();
-  const { _uid, isLogged } = useAuth();
-  const { isLoading, data, error } = useQuery(
-    'get visitor bonuses',
-    () => VisitorService.getUserBonuses(_uid),
-    {
-      enabled: isLogged,
-    },
-  );
 
   enum PagesEnum {
     bonuses, history, personalData
   }
   // prettier-ignore
   const [page, setPage] = useState<PagesEnum>(0);
-
-  const getTotalBonuses = (): number => {
-    if (data) {
-      return summary(...data?.data.map((bonus: Bonus) => bonus.bonus_amount));
-    } else return 0;
-  };
 
   return (
     <div
@@ -82,40 +63,7 @@ const VisitorAccount: FC<VisitorAccountProps> = ({}) => {
 
       <div className={cn(styles.divider)}></div>
 
-      {page === PagesEnum.bonuses && (
-        <>
-          {isLoading ? (
-            <div className={cn(styles.serverMessagePlaceholder)}>
-              <CircleLoader />
-            </div>
-          ) : !error && <>
-            <div className={cn(styles.totalBonuses)}>
-              <h4>
-                {loc.accountPage.visitor.totalBonusesText.replace(
-                  'XXXX',
-                  `${getTotalBonuses()}`,
-                )}
-              </h4>
-            </div>
-
-            <div className={cn(styles.bonusList)}>
-              {data?.data.map((bonus: Bonus, index: number) => (
-                <VisitorBonus bonus={bonus} key={`bonus-${index}`} />
-              ))}
-            </div>
-          </>}
-
-          {error && (
-            <div className={cn(styles.serverMessagePlaceholder)}>
-              {(error as AxiosError).code === 'ERR_NETWORK'
-                && loc.accountPage.messages.noConnectionToServer}
-
-              {(error as AxiosError).code === '500'
-                && loc.accountPage.messages.internalServerErrorMessage}
-            </div>
-          )}
-        </>
-      )}
+      {page === PagesEnum.bonuses && <BonusesTab />}
     </div>
   );
 };
