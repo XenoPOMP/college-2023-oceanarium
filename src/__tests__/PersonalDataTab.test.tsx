4f91548d7fd69d9cd9010848ebb08b903941da6a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-03 18:12:41 +0300

    добавил snapshots в gitignore

--- src/__tests__/PersonalDataTab.test.tsx
+++ src/__tests__/PersonalDataTab.test.tsx
@@ -1,8 +1,19 @@
 import { describe, expect, test } from 'vitest';
 import skipTestCondition from '@utils/skipTestCondition';
+import renderWithProviders, { RenderOptions } from '@utils/renderWithProviders';
+import PersonalDataTab from '@tabs/PersonalDataTab/PersonalDataTab';
 
 describe.skipIf(skipTestCondition('FRONTEND'))('Personal data tab', () => {
+  const defaultOptions: RenderOptions = {
+    useRedux: true,
+    useQuery: true,
+  };
+
   test('Match snapshot', () => {
-    expect(1);
+    expect(
+      renderWithProviders(<PersonalDataTab />, {
+        ...defaultOptions,
+      }),
+    ).toMatchSnapshot();
   });
 });
