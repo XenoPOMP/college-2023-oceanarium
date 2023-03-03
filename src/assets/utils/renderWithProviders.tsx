7a7391981b1b5fef1cff0f6e0863b3c54339e425
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-03 17:56:34 +0300

    добавил wrapper для ReactQuery

--- src/assets/utils/renderWithProviders.tsx
+++ src/assets/utils/renderWithProviders.tsx
@@ -2,10 +2,12 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '@redux/index';
 import { render } from '@testing-library/react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export interface RenderOptions {
   useRedux?: boolean;
   useRouter?: boolean;
+  useQuery?: boolean;
 }
 
 type Wrapper = (ui: JSX.Element) => JSX.Element;
@@ -24,10 +26,24 @@ const wrapInRouter: Wrapper = (ui) => {
   );
 };
 
+const wrapInReactQuery: Wrapper = (ui) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+        // refetchInterval: 5000
+      },
+    },
+  });
+
+  return <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>;
+};
+
 const renderWithProviders = (ui: JSX.Element, options?: RenderOptions) => {
   const renderOptions: RenderOptions = {
     useRedux: false,
     useRouter: false,
+    useQuery: false,
     ...options,
   };
   let renderUi: JSX.Element = ui;
@@ -38,6 +54,10 @@ const renderWithProviders = (ui: JSX.Element, options?: RenderOptions) => {
 
   if (renderOptions.useRouter) {
     renderUi = wrapInRouter(renderUi);
+  }
+
+  if (renderOptions.useQuery) {
+    renderUi = wrapInReactQuery(renderUi);
   }
 
   return render(renderUi);
