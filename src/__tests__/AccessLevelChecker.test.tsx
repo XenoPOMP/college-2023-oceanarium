4568cea005999cd394b1635e22fc6ea50dc4b380
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-12 23:41:15 +0300

    написал тест

---
+++ src/__tests__/AccessLevelChecker.test.tsx
@@ -0,0 +1,26 @@
+import { describe, expect, test } from 'vitest';
+import skipTestCondition from '@utils/skipTestCondition';
+import renderWithProviders, { RenderOptions } from '@utils/renderWithProviders';
+import AccessLevelChecker from '@components/AccessLevelChecker/AccessLevelChecker';
+
+describe.skipIf(skipTestCondition('FRONTEND'))(
+  'Access level check overlay',
+  () => {
+    const defaultConfig: RenderOptions = {
+      useRedux: true,
+      useRouter: true,
+    };
+
+    test('Match snapshot', () => {
+      expect(
+        renderWithProviders(<AccessLevelChecker preferredRole={'visitor'} />, {
+          ...defaultConfig,
+        }),
+      ).toMatchSnapshot();
+    });
+
+    test('Body scroll property change', () => {
+      expect(document.body.style.overflowY).toBe('hidden');
+    });
+  },
+);
