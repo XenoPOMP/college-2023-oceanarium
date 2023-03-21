72906c52179d54eba51f33f24f59afe1eba18f3d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-21 17:56:11 +0300

    добавил тудушку

--- src/assets/components/ui/BurgerMenu/BurgerMenu.tsx
+++ src/assets/components/ui/BurgerMenu/BurgerMenu.tsx
@@ -3,6 +3,8 @@ import { FC } from 'react';
 import styles from './BurgerMenu.module.scss';
 import { BurgerMenuProps } from './BurgerMenu.props';
 
+// TODO Сделать бургер меню
+
 const BurgerMenu: FC<BurgerMenuProps> = ({}) => {
   return <div className={cn(styles.burgerMenu)}>Burger menu</div>;
 };
