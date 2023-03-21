76f3573dd3f5d650938726bc324c64966c8da90a
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-22 00:17:55 +0300

    добавил строчку для отладки

--- src/assets/utils/ApiClient.ts
+++ src/assets/utils/ApiClient.ts
@@ -4,6 +4,8 @@ import useEnv from '@hooks/useEnv';
 const ApiClient = (): AxiosInstance => {
   const { API_URL } = useEnv();
 
+  console.log(API_URL);
+
   return axios.create({
     baseURL: API_URL,
   });
