c64bb572ba327cfc595a94398a4e1c8028eb2997
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-22 02:21:48 +0300

    добавил переключатель в бургер меню

--- src/assets/components/ui/BurgerMenu/BurgerMenu.tsx
+++ src/assets/components/ui/BurgerMenu/BurgerMenu.tsx
@@ -2,11 +2,72 @@ import cn from 'classnames';
 import { FC } from 'react';
 import styles from './BurgerMenu.module.scss';
 import { BurgerMenuProps } from './BurgerMenu.props';
+import useBoolean from '@hooks/useBoolean';
+import { motion, Transition } from 'framer-motion';
 
 // TODO Сделать бургер меню
 
 const BurgerMenu: FC<BurgerMenuProps> = ({}) => {
-  return <div className={cn(styles.burgerMenu)}>Burger menu</div>;
+  const { isToggled, toggle } = useBoolean(false);
+
+  const iconAnimationTransition: Transition = {
+    duration: 0.15,
+    ease: 'easeOut',
+  };
+
+  return (
+    <div className={cn(styles.burgerMenu)}>
+      <div className={cn(styles.revealButton)} onClick={toggle}>
+        <svg
+          width='18'
+          height='18'
+          viewBox='0 0 18 18'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <rect width='18' height='18' fill='transparent' />
+          <motion.rect
+            animate={{
+              rotate: isToggled ? 45 : 0,
+              y: isToggled ? '30%' : '0',
+            }}
+            transition={iconAnimationTransition}
+            x='1'
+            y='3'
+            width='16'
+            height='2'
+            rx='1'
+            fill='black'
+          />
+          <motion.rect
+            animate={{
+              opacity: isToggled ? 0 : 1,
+            }}
+            transition={iconAnimationTransition}
+            x='1'
+            y='8'
+            width='16'
+            height='2'
+            rx='1'
+            fill='black'
+          />
+          <motion.rect
+            animate={{
+              rotate: isToggled ? -45 : 0,
+              y: isToggled ? '-25%' : '0',
+            }}
+            transition={iconAnimationTransition}
+            x='1'
+            y='13'
+            width='16'
+            height='2'
+            rx='1'
+            fill='black'
+          />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default BurgerMenu;
