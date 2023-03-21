dbbe00d1f644c98ecb2b3479f38eec304536534d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-03-22 00:07:39 +0300

    занес вызов хука внутрь скобок

--- src/assets/utils/ApiClient.ts
+++ src/assets/utils/ApiClient.ts
@@ -1,10 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import useEnv from '@hooks/useEnv';
 
-const { API_URL } = useEnv();
+const ApiClient = (): AxiosInstance => {
+  const { API_URL } = useEnv();
 
-const ApiClient = axios.create({
-  baseURL: API_URL,
-});
+  return axios.create({
+    baseURL: API_URL,
+  });
+};
 
 export default ApiClient;
--- src/services/Visitor.service.ts
+++ src/services/Visitor.service.ts
@@ -2,19 +2,19 @@ import ApiClient from '@utils/ApiClient';
 
 export const VisitorService = {
   async getUserBonuses(_uid?: number) {
-    return ApiClient.post(`visitors/bonuses`, {
+    return ApiClient().post(`visitors/bonuses`, {
       _uid,
     });
   },
 
   async getUserVisits(_uid?: number) {
-    return ApiClient.post(`visitors/visits`, {
+    return ApiClient().post(`visitors/visits`, {
       _uid,
     });
   },
 
   async getUserData(_uid?: number) {
-    return ApiClient.post(`visitors/data`, {
+    return ApiClient().post(`visitors/data`, {
       _uid,
     });
   },
